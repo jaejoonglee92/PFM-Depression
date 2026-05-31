798c9c171e147e7cb47a2bd1333e9f6181e0734f
jaejoonglee92 <jaejoonglee92@gmail.com>
2026-06-01 02:16:44 +0900

    new parse script; add prior cifti files

---
+++ PFM-Tutorial/Utilities/pfm_parse_manual_decisions_filter.m
@@ -0,0 +1,92 @@
+function pfm_parse_manual_decisions_filter(Ic,Col,MidthickSurfs,Priors,XLS,OutFile,OutDir,WorkbenchBinary,MinSize)
+% cjl; cjl2007@med.cornell.edu;
+
+% read in 
+% infomap
+% communities 
+if ischar(Ic)
+    Ic = ft_read_cifti_mod(Ic); % Ic == infomap communities
+end
+
+% extract
+% graph density
+% of interest ;
+if ~isempty(Col)
+    Ic.data = Ic.data(:,Col);
+end
+
+% unique infomap communities;
+uCi = unique(nonzeros(Ic.data));
+
+% read in the labels;
+Labels = readtable(XLS);
+if isnumeric(Labels.Network_ManualDecision)
+    Labels.Network_ManualDecision = repmat({''}, height(Labels), 1);
+end
+wh_sconly = Labels.FC_Similarity == 0 & Labels.Spatial_Score == 0;
+Labels.Network(wh_sconly) = {'Noise'};
+
+O = Ic; % preallocate output
+O.data = zeros(size(Ic.data));
+
+% sweep through
+% the communities;
+for i = 1:length(uCi)
+    
+    % check to see if a manual decision was made;
+    if isempty(Labels.Network_ManualDecision{i}) 
+        
+        % log the network identity;
+        O.data(Ic.data(:,1)==uCi(i),1) = find(strcmp(Priors.NetworkLabels,Labels.Network{i}));
+        
+    else
+        
+        % only if the specified name matches a prior label;
+        if ismember(Labels.Network_ManualDecision{i},Priors.NetworkLabels)
+            
+            % log the network identity;
+            O.data(Ic.data(:,1)==uCi(i),1) = find(strcmp(Priors.NetworkLabels,Labels.Network_ManualDecision{i}));
+            
+        end
+        
+    end
+    
+end
+
+% write out the labeled (pre-filtering) cifti file;
+%O.data(O.data==find(strcmp(Priors.NetworkLabels,'Noise')))=0;
+ft_write_cifti_mod([OutDir '/Tmp.dtseries.nii'],O);
+
+% spatial filtering on the labeled network map (post-matching):
+% per-network removal of small clusters + nearest-neighbor dilation.
+% note: this operates on network labels, so adjacent same-network
+% communities are merged before the size threshold is applied.
+if MinSize > 0
+    pfm_spatial_filtering([OutDir '/Tmp.dtseries.nii'],OutDir,'Tmp.dtseries.nii',MidthickSurfs,MinSize,WorkbenchBinary);
+end
+
+% write out the first network;
+system(['echo ' char(Priors.NetworkLabels{1}) ' > ' OutDir '/LabelListFile.txt ']);
+system(['echo 1 ' num2str(round(Priors.NetworkColors(1,1)*255)) ' ' num2str(round(Priors.NetworkColors(1,2)*255)) ' ' num2str(round(Priors.NetworkColors(1,3)*255)) ' 255 >> ' OutDir '/LabelListFile.txt ']);
+
+% sweep through the networks;
+for i = 2:length(Priors.NetworkLabels)
+    
+    system(['echo ' char(Priors.NetworkLabels{i}) ' >> ' OutDir '/LabelListFile.txt ']);
+    system(['echo ' num2str(i) ' ' num2str(round(Priors.NetworkColors(i,1)*255)) ' ' num2str(round(Priors.NetworkColors(i,2)*255)) ' ' num2str(round(Priors.NetworkColors(i,3)*255)) ' 255 >> ' OutDir '/LabelListFile.txt ']);
+    
+end
+
+% make dense label file + network borders;
+system([WorkbenchBinary ' -cifti-label-import ' OutDir '/Tmp.dtseries.nii ' OutDir '/LabelListFile.txt ' OutDir '/' OutFile '.dlabel.nii -discard-others']);
+system([WorkbenchBinary ' -cifti-label-to-border ' OutDir '/' OutFile '.dlabel.nii -border ' MidthickSurfs{1} ' ' OutDir '/' OutFile '.L.border']); % LH
+system([WorkbenchBinary ' -cifti-label-to-border ' OutDir '/' OutFile '.dlabel.nii -border ' MidthickSurfs{2} ' ' OutDir '/' OutFile '.R.border']); % RH
+
+% remove some intermediate files;
+system(['rm ' OutDir '/Tmp.dtseries.nii']);
+system(['rm ' OutDir '/LabelListFile.txt']);
+
+end
+
+
+
